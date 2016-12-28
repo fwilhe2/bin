20a15a75912ff0ae19107c05f378a4d25a276fc6
Florian Wilhelm <florian-wilhelm@onlinehome.de>
2016-12-28 09:25:07 +0100

    Include methods for individual features

--- asciidoctor-playground.rb
+++ asciidoctor-playground.rb
@@ -2,55 +2,62 @@
 
 require 'asciidoctor'
 
-topics = Hash.new
 base_dir = ARGV[0]
 
 def output_file_name(input_file_name)
     return File.basename(input_file_name, '.*') + '.html'
 end
 
-Dir.glob base_dir + '/docs/**/*.adoc' do |doc|
-    Asciidoctor.convert_file doc, base_dir: base_dir, mkdirs: true, to_dir: 'output'
-
-    # collect topics to create index files later
-    asciidoctor_document = Asciidoctor.load_file doc
-    this_topics = asciidoctor_document.attributes['topics'].split(", ")
-    this_topics.each do |t|
-        if topics[t].nil?
-            topics[t] = []
-        end
-        topics[t] << output_file_name(doc)
+def convert_directory(base_dir)
+    Dir.glob base_dir + '/docs/**/*.adoc' do |doc|
+        Asciidoctor.convert_file doc, base_dir: base_dir, mkdirs: true, to_dir: 'output'
     end
-
 end
 
-meta_index = <<END
+def create_index_files(base_dir)
+    topics = Hash.new
+    Dir.glob base_dir + '/docs/**/*.adoc' do |doc|
+        asciidoctor_document = Asciidoctor.load_file doc
+        this_topics = asciidoctor_document.attributes['topics'].split(", ")
+        this_topics.each do |t|
+            if topics[t].nil?
+                topics[t] = []
+            end
+            topics[t] << output_file_name(doc)
+        end
+
+    end
+
+    meta_index = <<END
 = Index
 
 END
 
-def asciidoc_link(ref, text)
-    "link:#{ref}[#{text}]"
-end
+    def asciidoc_link(ref, text)
+        "link:#{ref}[#{text}]"
+    end
 
-# create index files
-topics.each do |t, docs|
-    meta_index += "#{asciidoc_link(t + '-index.html', t)}\n\n"
-    index = <<END
+    # create index files
+    topics.each do |t, docs|
+        meta_index += "#{asciidoc_link(t + '-index.html', t)}\n\n"
+        index = <<END
 == Index of #{t}
 
 END
-    docs.each do |doc|
-        index += "#{asciidoc_link(doc, File.basename(doc, '.*'))}\n\n"
+        docs.each do |doc|
+            index += "#{asciidoc_link(doc, File.basename(doc, '.*'))}\n\n"
+        end
+        html = Asciidoctor.convert index, header_footer: true
+        File.open(base_dir + '/output/' + t + '-index.html', 'w') do |f|
+            f.write(html)
+        end
+
     end
-    html = Asciidoctor.convert index, header_footer: true
-    File.open(base_dir + '/output/' + t + '-index.html', 'w') do |f|
+    html = Asciidoctor.convert meta_index, header_footer: true
+    File.open(base_dir + '/output/index.html', 'w') do |f|
         f.write(html)
     end
-
-end
-html = Asciidoctor.convert meta_index, header_footer: true
-File.open(base_dir + '/output/index.html', 'w') do |f|
-    f.write(html)
 end
 
+convert_directory(base_dir)
+create_index_files(base_dir)
